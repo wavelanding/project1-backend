266b2134a3c7e598493d47fdcef5f952d5fadb6e
Lizhou Yuan <lizhouyuan@Lizhous-MacBook-Pro.local>
2024-02-25 22:12:58 -0800

    first commit

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { TokenPayload } from '../token-payload.interface';
+import { getJwt } from '../jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          if (request.cookies.Authentication) {
+            return request.cookies.Authentication;
+          }
+          return getJwt(request.headers.authorization);
+        },
+      ]),
+      secretOrKey: configService.getOrThrow('JWT_SECRET'),
+    });
+  }
+
+  validate(payload: TokenPayload) {
+    return payload;
+  }
+}
